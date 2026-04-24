42a9b0c68be6d2f6307132bb4115b1da20329829
La Min Aung <laminaung.dev@gmail.com>
2026-04-25 02:08:43 +0700

    Feat: Add OrderPlaced consumer with logging and retry policies

--- FoodDelivery.Restaurant.Service/Program.cs
+++ FoodDelivery.Restaurant.Service/Program.cs
@@ -1,6 +1,42 @@
+using FoodDelivery.Restaurant.Service.Consumers;
+using FoodDelivery.Shared.Contracts.Events;
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<OrderPlacedConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Message<OrderPlacedEvent>(m => m.SetEntityName("order.created"));
+
+        var rabbitConfig = builder.Configuration.GetSection("RabbitMq");
+
+        cfg.Host(rabbitConfig["Host"], rabbitConfig["VirtualHost"], h =>
+        {
+            h.Username(rabbitConfig["Username"]);
+            h.Password(rabbitConfig["Password"]);
+        });
+
+        cfg.UseMessageRetry(r =>
+        {
+            r.Exponential(
+            retryLimit: 5,
+            minInterval: TimeSpan.FromSeconds(1),
+            maxInterval: TimeSpan.FromSeconds(30),
+            intervalDelta: TimeSpan.FromSeconds(2)
+            );
+
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Restaurant Worker Service is running and listening to RabbitMQ...");
 
 app.Run();
