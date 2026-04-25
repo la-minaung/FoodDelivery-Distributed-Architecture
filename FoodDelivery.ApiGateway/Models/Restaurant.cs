d058a36264c067e89c09e073fd71687a12a51dfe
La Min Aung <laminaung.dev@gmail.com>
2026-04-25 17:05:44 +0700

    Feat: Setup API Gateway with GraphQL and HotChocolate mock service (#7)

---
+++ FoodDelivery.ApiGateway/Models/Restaurant.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodDelivery.ApiGateway.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<MenuItem> Menu { get; set; } = new();
+    }
+}
---
+++ FoodDelivery.ApiGateway/Queries/Query.cs
@@ -0,0 +1,18 @@
+﻿using FoodDelivery.ApiGateway.Models;
+using FoodDelivery.ApiGateway.Services;
+
+namespace FoodDelivery.ApiGateway.Queries
+{
+    public class Query
+    {
+        public Restaurant GetRestaurantDetails([Service] RestaurantMockService restaurantService)
+        {
+            return restaurantService.GetMockRestaurant();
+        }
+
+        public MenuItem GetMenuItemById(int id, [Service] RestaurantMockService restaurantService)
+        {
+            return restaurantService.GetMenuItemById(id);
+        }
+    }
+}
