5f3897f398900f53d3d82876b0688187e2244ef7
La Min Aung <laminaung.dev@gmail.com>
2026-04-26 17:14:09 +0700

    Feat: Integrate gRPC client with GraphQL BFF for data aggregation

--- FoodDelivery.ApiGateway/Program.cs
+++ FoodDelivery.ApiGateway/Program.cs
@@ -1,9 +1,15 @@
 using FoodDelivery.ApiGateway.Queries;
 using FoodDelivery.ApiGateway.Services;
+using FoodDelivery.Shared.Contracts.gRPC;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<RestaurantMockService>();
+builder.Services.AddGrpcClient<RestaurantMenu.RestaurantMenuClient>(options =>
+{
+    options.Address = new Uri("https://localhost:7139");
+});
+
+builder.Services.AddScoped<RestaurantGrpcClientService>();
 
 builder.Services
     .AddGraphQLServer()
